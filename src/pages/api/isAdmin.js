5000872f84185b63950d5173e45969132d858fc9
viniciusjose-next <vinicius.souza@next.tec.br>
2023-09-14 19:20:07 -0300

    refactor: validation isAdmin completed

--- src/pages/api/isAdmin.js
+++ src/pages/api/isAdmin.js
@@ -1,11 +1,9 @@
 import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
-  const session = await getSession(req);
+  const session = await getSession({ req });
 
-  session.user.is_admin
-    ? res.redirect(300, '/adiministrador')
-    : res.redirect(300, '/usuario');
-
-  return;
+  return session.user.is_admin
+    ? res.redirect(302, '/administrador')
+    : res.redirect(302, '/usuario');
 }
