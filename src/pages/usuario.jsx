c38ca717ea05d6e76f6a1d2e08b9c5d235c89caa
Vinicius-1307 <viniciusjose9@outlook.com>
2023-09-18 23:30:00 -0300

    feat: implementig read messages route

--- src/pages/usuario.jsx
+++ src/pages/usuario.jsx
@@ -1,10 +1,8 @@
-import Button from '@/components/atoms/Button';
-import InputComponent from '@/components/atoms/Input';
-import CheckBox from '@/components/atoms/CheckBox';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
+import Button from '@/components/atoms/Button';
 
 export default function User() {
   const [messages, setMessages] = useState([]);
@@ -29,9 +27,29 @@ export default function User() {
     }
   }, [session]);
 
+  const handleCheckedMessage = async (event) => {
+    event.preventDefault();
+    await axios
+      .patch('http://127.0.0.1:8000/api/messages/${messsage.id}', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      })
+      .then((response) => {
+        alert(response.data.message);
+        router.reload();
+      })
+      .catch((error) => {
+        if (error) {
+          alert(error.data.message);
+        }
+        router.reload();
+      });
+  };
+
   return (
     <>
-      {console.log(messages)}
       <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
         <h1 className="p-10 text-4xl font-black text-blue-500">
           Message - {session?.user.name}
@@ -41,7 +59,17 @@ export default function User() {
           <div>
             {messages && messages.length > 0 ? (
               messages.map((message, index) => (
-                <input key={index} type="text" {...message.messages} />
+                <li
+                  key={index}
+                  className="flex flex-col list-none text-left border-b-2 mb-2"
+                >
+                  <label>
+                    <input type="checkbox" checked={checked[message.id]} />{' '}
+                    <br />
+                    <span>Título: {message.title}</span> <br />
+                    <span>Conteúdo: {message.content}</span>
+                  </label>
+                </li>
               ))
             ) : (
               <p className="text-lg text-blue-500 text-center">
@@ -49,6 +77,12 @@ export default function User() {
               </p>
             )}
           </div>
+          <Button
+            color="bg-blue-500"
+            onClick={() => handleCheckedMessage(message.id)}
+          >
+            Marcar como lida
+          </Button>
         </div>
       </div>
     </>
