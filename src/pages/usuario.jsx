647f1657d15ac06d93b5c9a39650a4105333e047
Vinicius-1307 <viniciusjose9@outlook.com>
2023-09-25 22:08:26 -0300

    refactor: validation of when the user has a message or not

--- src/pages/usuario.jsx
+++ src/pages/usuario.jsx
@@ -43,6 +43,7 @@ export default function User({ GetAllMessages }) {
 
   return (
     <>
+      {console.log(GetAllMessages)}
       <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
         <h1 className="p-10 text-4xl font-black text-blue-500">
           Message - {session?.user.name}
@@ -50,34 +51,29 @@ export default function User({ GetAllMessages }) {
         <div className="px-3.5 h-max w-96 bg-white rounded-lg border-solid border-2 border-gray-200 shadow-lg">
           <h2 className="py-4 flex justify-center text-xl mb-3">Mensagens</h2>
           <div>
-            {GetAllMessages && GetAllMessages.readed !== 1 ? (
-              GetAllMessages.filter((message) => message.readed !== 1).map(
-                (message, index) => (
-                  <li
-                    key={index}
-                    className="list-none text-left border-b-2 mb-2"
-                  >
-                    <label className="flex flex-col relative">
-                      <div className="flex flex-col">
-                        <span>Título: {message.title}</span>
-                        <span>Conteúdo: {message.content}</span>
-                      </div>
-                      <input
-                        className="absolute right-4 top-4"
-                        type="checkbox"
-                        onClick={(e) => {
-                          if (e.target.checked) {
-                            handleCheckBox({ id: message.id });
-                          } else {
-                            handleCheckBox({ id: message.id, isRemoved: true });
-                          }
-                        }}
-                        checked={message.isSelected}
-                      />
-                    </label>
-                  </li>
-                ),
-              )
+            {GetAllMessages?.length > 0 ? (
+              GetAllMessages.map((message, index) => (
+                <li key={index} className="list-none text-left border-b-2 mb-2">
+                  <label className="flex flex-col relative">
+                    <div className="flex flex-col">
+                      <span>Título: {message.title}</span>
+                      <span>Conteúdo: {message.content}</span>
+                    </div>
+                    <input
+                      className="absolute right-4 top-4"
+                      type="checkbox"
+                      onClick={(e) => {
+                        if (e.target.checked) {
+                          handleCheckBox({ id: message.id });
+                        } else {
+                          handleCheckBox({ id: message.id, isRemoved: true });
+                        }
+                      }}
+                      checked={message.isSelected}
+                    />
+                  </label>
+                </li>
+              ))
             ) : (
               <p className="text-lg text-blue-500 text-center">
                 Não há nenhuma mensagem para você, ou já foram marcadas como
@@ -85,14 +81,16 @@ export default function User({ GetAllMessages }) {
               </p>
             )}
           </div>
-          <div className="flex justify-center">
-            <button
-              className="bg-blue-500 w-40 h-12 text-white rounded-lg mb-2"
-              onClick={(event) => handleCheckedMessage(event)}
-            >
-              Marcar como lida
-            </button>
-          </div>
+          {GetAllMessages?.length > 0 && (
+            <div className="flex justify-center">
+              <button
+                className="bg-blue-500 w-40 h-12 text-white rounded-lg mb-2"
+                onClick={(event) => handleCheckedMessage(event)}
+              >
+                Marcar como lida
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
